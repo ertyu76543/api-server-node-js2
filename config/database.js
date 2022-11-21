bafe5ab444ae822c65e788324d53b529484602e9
{will} <{ertyu76543@naver.com}>
2022-11-21 20:57:30 +0900

    apt

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '52.78.161.96',
+    host: '52.79.163.178',
     user: 'will',
     port: '3306',
     password: '12345will',
